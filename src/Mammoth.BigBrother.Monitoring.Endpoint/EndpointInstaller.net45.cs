2bb0165f3c175eb6ce801c22a84d4d0e189e6768
Alessandro Giorgetti <alessandro.giorgetti@live.com>
2018-05-15 18:32:05 +0200

    added SelfHost Endpoint installer for .net 4.5 (with cors)

---
+++ src/Mammoth.BigBrother.Monitoring.Endpoint/EndpointInstaller.net45.cs
@@ -0,0 +1,52 @@
+﻿#if NET45
+
+using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Cors;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.Http.SelfHost;
+
+namespace Mammoth.BigBrother.Monitoring.Endpoint
+{
+    public static class EndpointInstaller
+    {
+        private static HttpSelfHostServer _webHost;
+        private static int _loopbackPort;
+
+        public static void Start(int loopbackPort)
+        {
+            _loopbackPort = loopbackPort;
+            _webHost = BuildWebHost(null);
+            _webHost.OpenAsync().Wait();
+        }
+
+        public static Task Stop()
+        {
+            // await _webHost.StopAsync().ConfigureAwait(false);
+            return Task.Delay(0);
+        }
+
+        private static HttpSelfHostServer BuildWebHost(string[] args)
+        {
+            var config = new HttpSelfHostConfiguration(IPAddress.Loopback + ":" + _loopbackPort);
+
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
+            //config.Routes.MapHttpRoute(
+            //    "CrossDomain", "crossdomain.xml",
+            //    new { controller = "CrossDomain" });
+
+            config.Routes.MapHttpRoute(
+                "API Default", "api/{controller}/{action}/{id}",
+                new { id = RouteParameter.Optional });
+
+            return new HttpSelfHostServer(config);
+        }
+
+    }
+}
+
+#endif
