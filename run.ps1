51e59e8df53a87a6f41d7ee5ca9ff13f0a465136
Alessandro Giorgetti <alessandro.giorgetti@live.com>
2018-08-10 09:50:38 +0200

    updated build scripts and libraries

--- run.ps1
+++ run.ps1
@@ -20,12 +20,12 @@ function Resolve-MsBuild {
 
 $msBuild = Resolve-MsBuild
 
-& $msBuild ".\src" /t:restore
-& $msBuild ".\src" /t:build
+& $msBuild ".\src" /t:restore /m
+& $msBuild ".\src" /t:build /m
 & Start-Process dotnet ".\src\Samples\Frontend\bin\Debug\netcoreapp2.0\Frontend.dll"
 & Start-Process "C:\Work\BigBrother\src\Samples\Frontend.net45\bin\Debug\Frontend.net45.exe"
 & Start-Process dotnet "run --no-restore --no-build --project .\src\Mammoth.BigBrother.Ui\Mammoth.BigBrother.Ui.csproj"
 
-Write-Host "Operation completed."
+#Write-Host "Operation completed."
 
-Read-Host
+#Read-Host
