ef835c6cc0f64a37a09a42a71a167b6eb1bc1aa1
Alessandro Giorgetti <alessandro.giorgetti@live.com>
2018-08-29 14:48:11 +0200

    Added some Router navigation actions and a router effect

--- src/Mammoth.BigBrother.Ui/Ui/src/app/store/effects/index.ts
+++ src/Mammoth.BigBrother.Ui/Ui/src/app/store/effects/index.ts
@@ -1,5 +1,6 @@
 import { ActorsEffects } from "./actors.effects";
+import { RouterEffects } from "../router/router";
 
-export const effects: any[] = [ActorsEffects];
+export const effects: any[] = [ActorsEffects, RouterEffects];
 
 export * from "./actors.effects";
--- src/Mammoth.BigBrother.Ui/Ui/src/app/store/router/router.ts
+++ src/Mammoth.BigBrother.Ui/Ui/src/app/store/router/router.ts
@@ -1,7 +1,11 @@
+import { Location } from "@angular/common";
 import { Injectable } from "@angular/core";
-import { Params, RouterStateSnapshot } from "@angular/router";
+import { NavigationExtras, Params, Router, RouterStateSnapshot } from "@angular/router";
+import { Actions, Effect } from "@ngrx/effects";
 import { RouterStateSerializer } from "@ngrx/router-store";
+import { Action } from "@ngrx/store";
 import { IAppState } from "../state";
+import { tap } from "rxjs/operators";
 
 export interface IRouterStateUrl {
   url: string;
@@ -27,4 +31,61 @@ export class CustomRouterStateSerializer implements RouterStateSerializer<IRoute
     // instead of the entire snapshot
     return { url, params, queryParams };
   }
+}
+
+export enum RouterActionsTypes {
+  GOTO = "[router] goto",
+  BACK = "[router] back",
+  FORWARD = "[router] forward"
+}
+
+export class RouterGoTo implements Action {
+  readonly type = RouterActionsTypes.GOTO;
+  constructor(
+    public path: any[],
+    public query?: Params,
+    public extras?: NavigationExtras
+  ) { }
+}
+
+export class RouterBack implements Action {
+  readonly type = RouterActionsTypes.BACK;
+}
+
+export class RouterFoward implements Action {
+  readonly type = RouterActionsTypes.FORWARD;
+}
+
+export type RouterActions = RouterGoTo
+  | RouterBack
+  | RouterFoward;
+
+@Injectable()
+export class RouterEffects {
+
+  @Effect({ dispatch: false })
+  goto$ = this.actions$.ofType(RouterActionsTypes.GOTO)
+    .pipe(
+      tap((action: RouterGoTo) => {
+        this.router.navigate(action.path, { queryParams: action.query, ...action.extras });
+      })
+    );
+
+  @Effect({ dispatch: false })
+  back$ = this.actions$.ofType(RouterActionsTypes.BACK)
+    .pipe(
+      tap(() => this.location.back())
+    );
+
+  @Effect({ dispatch: false })
+  forward$ = this.actions$.ofType(RouterActionsTypes.FORWARD)
+    .pipe(
+      tap(() => this.location.forward())
+    );
+
+  constructor(
+    private actions$: Actions,
+    private router: Router,
+    private location: Location
+  ) { }
 }
